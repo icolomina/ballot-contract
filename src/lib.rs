52f746b76f78ee01e21806174a9420a5c39ce482
Nacho <ignacio.colomina@freemediainternet.com>
2023-04-20 15:00:33 +0200

    Finish

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![no_std]
 
-use soroban_sdk::{contractimpl, contracttype, panic_with_error, Env, Symbol, Map, Address, Vec};
+use soroban_sdk::{contractimpl, contracttype, contracterror, panic_with_error, Env, Symbol, Map, Address, Vec};
 
 const VOTES: Symbol = Symbol::short("votes");
 const PARTIES: Symbol = Symbol::short("parties");
@@ -55,15 +55,24 @@ fn voter_is_delegated(env: &Env, voter: &Symbol) -> bool {
     dvts.contains(voter)
 }
 
-fn count_delegated_votes(env: &Env, d_voter_ckey: DCounter) -> u32 {
-    let v_dvts: u32 = env
+fn get_delegated_votes(env: &Env, d_voter: &Symbol) -> Vec<Symbol> {
+    let v_dvts: Vec<Symbol> = env
         .storage()
-        .get(&d_voter_ckey)
-        .unwrap_or(Ok(0))
+        .get(d_voter)
+        .unwrap_or(Ok(Vec::new(env)))
         .unwrap()
     ;
 
     v_dvts
+}
+
+fn voter_has_delegated_votes(env: &Env, voter: &Symbol) -> bool {
+    let dvotes = get_delegated_votes(env, voter);
+    if dvotes.len() > 0 {
+        return true;
+    }
+
+    false
 }
 
 #[contracttype]
@@ -71,9 +80,13 @@ pub enum VCounter {
     Counter(Symbol)
 }
 
-#[contracttype]
-pub enum DCounter {
-    Counter(Symbol)
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum Error {
+    VoterHasHisVoteDelegated = 1,
+    VoterHasAlreadyVoted = 2,
+    VoterHasDelegatedVotes = 3
 }
 
 pub struct Ballot;
@@ -85,18 +98,18 @@ impl Ballot {
         admin.require_auth();
 
         if voter_is_delegated(&env, &voter) {
-
+            panic_with_error!(&env, Error::VoterHasHisVoteDelegated);
         }
         if voter_has_voted(&env, &voter) {
-            
+            panic_with_error!(&env, Error::VoterHasAlreadyVoted);
         }
         
         store_party(&env, &party);
 
         let mut votes: Vec<Symbol> = get_votes(&env);
         let pckey = VCounter::Counter(party);
-        let dvot_del_counter = DCounter::Counter(voter.clone());
-        let count = 1 + count_delegated_votes(&env, dvot_del_counter) + env.storage().get(&pckey).unwrap_or(Ok(0)).unwrap();
+        let d_votes: Vec<Symbol> = get_delegated_votes(&env, &voter);
+        let count = 1 + d_votes.len() + env.storage().get(&pckey).unwrap_or(Ok(0)).unwrap();
         votes.push_back(voter);
  
         env.storage().set(&pckey, &count);
@@ -107,19 +120,27 @@ impl Ballot {
 
     pub fn delegate(env: Env,  admin: Address, o_voter: Symbol, d_voter: Symbol) -> bool {
         admin.require_auth();
-        if voter_is_delegated(&env, &d_voter) || voter_is_delegated(&env, &o_voter){
-
+        
+        if voter_is_delegated(&env, &o_voter) {
+            panic_with_error!(&env, Error::VoterHasHisVoteDelegated);
         }
 
-        let dvot_del_counter = DCounter::Counter(d_voter);
+        if voter_is_delegated(&env, &d_voter) {
+            panic_with_error!(&env, Error::VoterHasHisVoteDelegated);
+        }
+
+        if voter_has_delegated_votes(&env, &o_voter) {
+            panic_with_error!(&env, Error::VoterHasDelegatedVotes);
+        }
+
+
         let mut d_votes = env.storage().get(&DVOTES).unwrap_or(Ok(Vec::new(&env))).unwrap();
-        let mut d_vot_delegs: u32 = env.storage().get(&dvot_del_counter).unwrap_or(Ok(0)).unwrap();
+        let mut d_vot_delegs: Vec<Symbol> = env.storage().get(&d_voter).unwrap_or(Ok(Vec::new(&env))).unwrap();
         d_votes.push_back(o_voter.clone());
-        
-        d_vot_delegs += 1;
+        d_vot_delegs.push_back(o_voter.clone());
 
         env.storage().set(&DVOTES, &d_votes);
-        env.storage().set(&dvot_del_counter, &d_vot_delegs);
+        env.storage().set(&d_voter, &d_vot_delegs);
 
         true
 
